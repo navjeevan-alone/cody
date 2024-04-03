8928c021a90e4fd725bd6c8b0c1b1e435a9e76bc
Navjeevan Alone <navjeevanalone352@gmail.com>
2024-04-03 20:55:32 +0530

    added beautiful voice

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import requests
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice', voices[1].id)
 engine.setProperty('rate', 150)
 
 def speak(audio):
